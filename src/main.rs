1937e6d9ad3325834dad90bd8bbe5fb01bae6f49
laperex <krishnadev7777@gmail.com>
2024-05-11 11:36:00 +0530

    fini

--- src/main.rs
+++ src/main.rs
@@ -12,44 +12,66 @@ pub fn main() {
 	const height: u32 = 1080;
 	const box_width: u32 = 40;
 	
-    // let sdl_context = sdl2::init().unwrap();
-    // let video_subsystem = sdl_context.video().unwrap();
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
 
-    // let window = video_subsystem.window("rust-sdl2 demo", 1920, 1080)
-    //     .position_centered()
-    //     .build()
-    //     .unwrap();
+    let window = video_subsystem.window("rust-sdl2 demo", 1920, 1080)
+        .position_centered()
+        .build()
+        .unwrap();
 
-    // let mut canvas = window.into_canvas().build().unwrap();
+    let mut canvas = window.into_canvas().build().unwrap();
 
-    // canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 255, 255));
-    // canvas.clear();
-    // canvas.present();
+    canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
 
-    // let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+	let mut snake = rnake::SnakeLogic::new(width, height, box_width);
 
-	let mut snake = rnake::SnakeLogic::init(width, height, box_width);
+	let mut dir = rnake::Point::new(1, 0);
 
-	snake.update(rnake::Point::init(1, 0));
-
-    // 'running: loop {
-    //     canvas.clear();
-
-    //     for event in event_pump.poll_iter() {
-    //         match event {
-    //             Event::Quit {..} |
-    //             Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-    //                 break 'running
-    //             },
-    //             _ => {}
-    //         }
-    //     }
-    //     // The rest of the game loop goes here...
+    'running: loop {
+		canvas.clear();
 		
-		
+        for event in event_pump.poll_iter() {
+			match event {
+				Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => break 'running,
 
-    //     canvas.present();
+				Event::KeyDown { keycode, .. } =>
+					match keycode.unwrap() {
+						Keycode::W => {
+							if dir.y == 0 {
+								dir = rnake::Point::new(0, -1)
+							}
+						},
+						Keycode::A => {
+							if dir.x == 0 {
+								dir = rnake::Point::new(-1, 0)
+							}
+						},
+						Keycode::S => {
+							if dir.y == 0 {
+								dir = rnake::Point::new(0, 1)
+							}
+						},
+						Keycode::D => {
+							if dir.x == 0 {
+								dir = rnake::Point::new(1, 0)
+							}
+						},
+						_ => {}
+					}
+				_ => {}
+			}
+		}
 
-    //     std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
-    // }
+		snake.update(dir);
+			
+        snake.render(&mut canvas, rnake::Point::new(0, 0));
+
+        canvas.present();
+
+        std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 10));
+    }
 }
--- src/rnake.rs
+++ src/rnake.rs
@@ -1,14 +1,31 @@
-use std::ops::{Add, Index};
+use std::ops::{Add};
+
+use rand::Rng;
+use sdl2::{sys::INT32_MAX, video::Window};
 
 
 
 const SNAKE_MAXLEN: usize = 100;
 
 
-#[derive(Debug, Copy, Clone, PartialEq)]
+#[derive(Debug)]
 pub struct Point {
-	x: i32,
-	y: i32
+	pub x: i32,
+	pub y: i32
+}
+
+impl Copy for Point {}
+
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+		self.x == other.x && self.y == other.y
+	}
+}
+
+impl Clone for Point {
+    fn clone(&self) -> Self {
+		Self { x: self.x.clone(), y: self.y.clone() }
+	}
 }
 
 impl Add for Point {
@@ -17,14 +34,14 @@ impl Add for Point {
 	fn add(self, rhs: Self) -> Self::Output {
 		Self {
 			x: rhs.x + self.x,
-			y: rhs.x + self.x,
+			y: rhs.y + self.y,
 		}
 	}
 }
 
 
 impl Point {
-	pub fn init(x: i32, y: i32) -> Self {
+	pub fn new(x: i32, y: i32) -> Self {
 		Self {
 			x,
 			y,
@@ -46,106 +63,133 @@ pub struct SnakeLogic {
 	
 	width: u32,
 	height: u32,
-	
-	snake_dir: Point
 }
 
 
 impl SnakeLogic {
+	pub fn render(&self, canvas: &mut sdl2::render::Canvas<Window>, offset: Point) {
+		let mut rect = sdl2::rect::Rect::new(0, 0, self.box_width, self.box_width);
+		
+		canvas.set_draw_color(sdl2::pixels::Color::RGB(255, 0, 0));
+
+		
+		rect.x = self.apple.x * self.box_width as i32 + offset.x;
+		rect.y = self.apple.y * self.box_width as i32 + offset.y;
+
+		canvas.fill_rect(rect);
+		
+				
+		for i in 0..self.snake_len as usize {
+			let mut v: usize = i + self.head;
+			
+			v -= if v >= SNAKE_MAXLEN { SNAKE_MAXLEN } else { 0 };
+			
+			if v == self.head {
+				canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 255));
+			} else {
+				canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 255, 0));
+			}
+	
+			rect.x = self.snake[v].x * self.box_width as i32;
+			rect.y = self.snake[v].y * self.box_width as i32;
+
+			canvas.fill_rect(rect);
+		}
+		
+		canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+		
+		for x in (0..self.width as i32).step_by(self.box_width as usize) {
+			for i in 0..7 as i32 {
+				canvas.draw_line(sdl2::rect::Point::new(x + i, 0), sdl2::rect::Point::new(x + i, self.height as i32));
+			}
+		}
+		
+		for y in (0..self.height as i32).step_by(self.box_width as usize) {
+			for i in 0..7 as i32 {
+				canvas.draw_line(sdl2::rect::Point::new(0, y + i), sdl2::rect::Point::new(self.width as i32, y + i));
+			}
+		}
+
+		canvas.set_draw_color(sdl2::pixels::Color::RGB(0, 0, 0));
+	}
+	
 	pub fn apple_regen(&mut self) {
 		loop {
-			self.apple.x = rand::random::<i32>() % (self.width / self.box_width) as i32;
-			self.apple.y = rand::random::<i32>() % (self.height / self.box_width) as i32;
+			self.apple.x = (rand::thread_rng().gen_range(0..INT32_MAX) % (self.width / self.box_width)) as i32;
+			self.apple.y = (rand::thread_rng().gen_range(0..INT32_MAX) % (self.height / self.box_width)) as i32;
 
-			for el in self.snake {
-				if el == self.apple {
+			for i in 0..self.snake_len as usize{
+				let mut v: usize = i + self.head;
+				
+				v -= if v >= SNAKE_MAXLEN { SNAKE_MAXLEN } else { 0 };
+				
+				if self.snake[v] == self.apple {
 					continue;
 				}
 			}
 
-			return;
+			break;
 		}
 	}
 	
-	pub fn update(&mut self, mut dir: Point) {
+	pub fn update(&mut self, dir: Point) {
 		if self.snake_len == 0 {
 			self.snake_len = 5;
+			self.head = 0;
 
 			for i in 0..self.snake_len {
-				self.snake[(5 - i - 1) as usize].x = i + 1;
-				self.snake[(5 - i - 1) as usize].y = 2;
+				self.snake[(self.snake_len - i - 1) as usize].x = i + 1;
+				self.snake[(self.snake_len - i - 1) as usize].y = 2;
 			}
 			
 			self.apple_regen();
 		} else {
-			for i in 0..2 {
-				let mut new_head_p = self.snake[self.head] + dir;
-				
-				
-				if new_head_p.x > (self.width / self.box_width) as i32 - 1 {
-					new_head_p.x = 0;
-				}
-	
-				if new_head_p.y > (self.height / self.box_width) as i32  - 1 {
-					new_head_p.y = 0;
-				}
-	
-	
-				if new_head_p.x < 0 {
-					new_head_p.x = (self.width / self.box_width) as i32 - 1;
-				}
-	
-				if new_head_p.y < 0 {
-					new_head_p.y = (self.height / self.box_width) as i32  - 1;
-				}
-				
-				
-				if new_head_p.x != self.snake[self.head + 1].x || new_head_p.y != self.snake[self.head + 1].y {
-					self.head = if self.head > 0 { self.head - 1 } else { SNAKE_MAXLEN - 1 };
-					
-					self.snake[self.head] = new_head_p;
-					self.snake_dir = dir;
-				} else {
-					dir = self.snake_dir;
-				}
+			let n_head = if self.head > 0 { self.head - 1 } else { SNAKE_MAXLEN - 1 };
+			self.snake[n_head] = self.snake[self.head] + dir;
+			self.head = n_head;
+
+			if self.snake[n_head].x > (self.width / self.box_width) as i32 - 1 {
+				self.snake[n_head].x = 0;
 			}
-				
-			for i in 0..self.snake_len as usize {
+
+			if self.snake[n_head].y > (self.height / self.box_width) as i32  - 1 {
+				self.snake[n_head].y = 0;
+			}
+
+
+			if self.snake[n_head].x < 0 {
+				self.snake[n_head].x = (self.width / self.box_width) as i32 - 1;
+			}
+
+			if self.snake[n_head].y < 0 {
+				self.snake[n_head].y = (self.height / self.box_width) as i32  - 1;
+			}
+
+			for i in 1..self.snake_len as usize {
 				let mut v: usize = i + self.head;
 				
 				v -= if v >= SNAKE_MAXLEN { SNAKE_MAXLEN } else { 0 };
 				
 				if self.snake[v] == self.snake[self.head] {
+					println!("Game Over!");
 					
+					self.snake_len = 0;
+
+					return;
 				}
 			}
+			
+			if self.apple == self.snake[self.head] {
+				println!("points: {}", self.snake_len - 5);
 				
-				// for (int i = 1; i < snake_len; i++) {
-				// 	int v = i + head;
-
-				// 	v -= (v >= SNAKE_BUFFER_LIMIT) ? SNAKE_BUFFER_LIMIT: 0;
-					
-				// 	if (snake[v].x == snake[head].x && snake[v].y == snake[head].y) {
-				// 		printf("game over!\n");
-						
-				// 		snake_len = 0;
-						
-				// 		break;
-				// 	}
-				// }
-
-
-				// if (apple.x == snake[head].x && apple.y == snake[head].y) {
-				// 	snake_len += 1;
-					
-				// 	apple_regen();
-					
-				// 	printf("points: %d\n", snake_len - 5);
-				// }
+				self.snake_len += 1;
+				
+				self.apple_regen();
+			}
 		}
 	}
 
-	pub fn init(width: u32, height: u32, box_width: u32) -> Self {
+	pub fn new(width: u32, height: u32, box_width: u32) -> Self {
 		Self {
 			head: 0,
 			
@@ -158,8 +202,6 @@ impl SnakeLogic {
 
 			width,
 			height,
-			
-			snake_dir: Point { x: 1, y: 0 },
 		}
 	}
 }
